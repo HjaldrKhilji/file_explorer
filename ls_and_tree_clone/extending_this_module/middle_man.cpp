f04127688f6c51f4c8d71eeb85b59a04b1f391f7
Mueem <rusafkakhilji@gmail.com>
2026-09-13 01:23:22 +0500

    Update search output functions in middle_man.cpp
    Refactor search output functions to include error checking.

--- ls_and_tree_clone/extending_this_module/middle_man.cpp
+++ ls_and_tree_clone/extending_this_module/middle_man.cpp
@@ -2,6 +2,7 @@
 #include<string>
 import list_filter_and_format;
 using content_t=list_filter_and_format::directory_content_t;
+using searcher=list_filter_and_format::searcher;
 namespace posix{
     extern "C"{
         #include <unistd.h>
@@ -11,19 +12,30 @@ namespace posix{
 }
 export storage_for_driver;
 export namespace storage_for_driver{
-    inline void output_search_and_discard_data_inside_current_dir(){
-        content_t temp= list_content();
+    inline void output_search(){
+        content_t temp= list_filter_and_format::list_content();
         //print content and done
     }
-    using storage_t=std::vector<value_type>;
+    inline void output_search_while_checking_errors(){
+        content_t temp= list_filter_and_format::list_content_while_checking_errors();
+        //print content and done
+    }
+    using storage_t=std::vector<searcher>;
 
     class dynamic_searcher_list: public storage{
-        inline void output_search_and_discard_data_through_storage(){
+        inline void output_search_inside_current_dir(){
             for(auto dir : storage){
                 content_t temp= dir.list_content();
                 //print content and done
             }
         }
+        inline void output_search_inside_current_dir(){
+            for(auto dir : storage){
+                content_t temp= dir.list_content_while_checking_errors();
+                //print content and done
+            }
+        }
     };
+
 
 };
