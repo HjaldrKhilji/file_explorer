ee879bf08dd6e17d72a57820b682c7a47c98e64f
Mueem <rusafkakhilji@gmail.com>
2026-09-12 21:19:31 +0500

    Add inline specifier to methods in ls_command_clone

--- ls_and_tree_clone/ls_command_clone.cpp
+++ ls_and_tree_clone/ls_command_clone.cpp
@@ -23,19 +23,19 @@ export namespace list_filter_and_format {
     struct directory_content_t{
         dirent **restrict dir_list;
         std::size_t lenght;
-        directory_content_t(directory_content_t&&)=default;
-        void operator=(directory_content_t&&)=default;
+        inline directory_content_t(directory_content_t&&)=default;
+        inline void operator=(directory_content_t&&)=default;
         //for simplicity, no copying because none is needed
-        void print_content(std::ostream){
+        inline void print_content(std::ostream){
             //todo
         }
-        ~directory_content_t(){
+        inline ~directory_content_t(){
         for(int i=0; i<content.lenght; i++){
             free(content.dir_list[i]);
         }
         }
     };
-    directory_content_t list_content(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
+    inline directory_content_t list_content(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
         directory_content_t result;
         result.lenght=scandir(path.c_str(), &result.dir_list, filter_list[index_for_filter], ordering_list[index_for_ordering]);
         return result;
@@ -44,13 +44,13 @@ export namespace list_filter_and_format {
     public:
         std::array<main_function_type> filter_list{&filter_non};
         std::array<main_ordering_type> ordering_list{&total_no_op_order};
-        driver(int d){
+        inline driver(int d){
         directory_fd=d;
         }
-        driver(std::string path_name){
+        inline driver(std::string path_name){
             fd= open(path_name.c_str(), O_DIRECTORY);
         }
-        directory_content_t list_content(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
+        inline directory_content_t list_content(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
             directory_content_t result;
             result.lenght=scandirat(directory_fd, path.c_str(), &result.dir_list, filter_list[index_for_filter], ordering_list[index_for_ordering]);
             return result;
