41ca2e212e7cdca0e48f0a953b1b09116cf6d668
Mueem <rusafkakhilji@gmail.com>
2026-09-13 00:31:53 +0500

    Update and rename make_use_of_and_discard_of_resources.cpp to middle_man.cpp

--- ls_and_tree_clone/extending_this_module/middle_man.cpp
+++ ls_and_tree_clone/extending_this_module/middle_man.cpp
@@ -15,27 +15,15 @@ export namespace storage_for_driver{
         content_t temp= list_content();
         //print content and done
     }
-    class dynamic_searcher_list{
-        inline dynamic_searcher_list()=default;
-        inline void add(value_type element){
-            storage.push_back(element);
-        }
+    using storage_t=std::vector<value_type>;
+
+    class dynamic_searcher_list: public storage{
         inline void output_search_and_discard_data_through_storage(){
             for(auto dir : storage){
                 content_t temp= dir.list_content();
                 //print content and done
             }
         }
-
-        inline value_type& operator[](size_type index){
-            return storage[index];
-        }
-    private:
-        storage_t storage;
-
-        using storage_t=std::vector<value_type>;
-        using value_type=storage_t::value_type;
-        using size_type=storage_t::size_type;
     };
 
 };
