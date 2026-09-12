0593a9d5937db497b390200d5fb779ba94a9a6b6
Mueem <rusafkakhilji@gmail.com>
2026-09-13 01:20:16 +0500

    Enhance directory listing with error handling
    Added error checking and improved output for directory listing.

--- ls_and_tree_clone/ls_command_clone.cpp
+++ ls_and_tree_clone/ls_command_clone.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-#include<string>
-#include<array>
+#include <string>
+#include <array>
+#include <system_error>
+#include <print>
+#include <locale>
+
 //tree must be implemented in some other file and it would probably require a different file, where it tries to make use of the whole project in order to accomplish the desired task
 namespace posix{
     extern "C"{
@@ -35,12 +39,23 @@ export namespace list_filter_and_format {
         }
         }
     };
-    inline directory_content_t list_content(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
+inline directory_content_t list_content(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
         directory_content_t result;
         result.lenght=scandir(path.c_str(), &result.dir_list, filter_list[index_for_filter], ordering_list[index_for_ordering]);
         return result;
     }
-    class searcher{
+inline directory_content_t list_content_while_checking_errors(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
+        directory_content_t result = list_content(path, index_for_filter, index_for_ordering);
+        if(result==-1){
+            const std::error_condition econd =
+            std::system_category().default_error_condition(errno);
+            std::locale::global(std::locale(""));
+            std::cerr.imbue(std::locale());//pretends your system is using utf-8 which most systems do (println) dosent support std::wcerr or std::wcout since they are wostreams rather than ostreams(println only accepts this).
+            std::println(std::cerr, "Category:     {}\nValue:   {}\nMessage:  {}\n\n",
+                         econd.category().name(), econd.value(), econd.message());
+        }
+    }
+class searcher{
     public:
         std::array<main_function_type> filter_list{&filter_non};
         std::array<main_ordering_type> ordering_list{&total_no_op_order};
@@ -55,6 +70,18 @@ export namespace list_filter_and_format {
             result.lenght=scandirat(directory_fd, path.c_str(), &result.dir_list, filter_list[index_for_filter], ordering_list[index_for_ordering]);
             return result;
         }
+        inline directory_content_t list_content_while_checking_errors(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
+            directory_content_t result = list_content(path, index_for_filter, index_for_ordering);
+            if(result==-1){
+                const std::error_condition econd =
+                std::system_category().default_error_condition(errno);
+                std::locale::global(std::locale(""));
+                std::cerr.imbue(std::locale());//pretends your system is using utf-8 which most systems do (println) dosent support std::wcerr or std::wcout since they are wostreams rather than ostreams(println only accepts this).
+                std::println(std::cerr, "Category:     {}\nValue:   {}\nMessage:  {}\n\n",
+                             econd.category().name(), econd.value(), econd.message());
+            }
+        }
+
         inline ~searcher(){
             close(directory_fd);
         }
