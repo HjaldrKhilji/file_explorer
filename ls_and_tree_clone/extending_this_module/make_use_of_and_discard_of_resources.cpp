b6e1050c833fc502de8f8056793504d4397eb1ac
Mueem <rusafkakhilji@gmail.com>
2026-09-12 21:19:46 +0500

    Update make_use_of_and_discard_of_resources.cpp

--- ls_and_tree_clone/extending_this_module/make_use_of_and_discard_of_resources.cpp
+++ ls_and_tree_clone/extending_this_module/make_use_of_and_discard_of_resources.cpp
@@ -11,21 +11,29 @@ namespace posix{
 }
 export storage_for_driver;
 export namespace storage_for_driver{
+    inline void output_search_and_discard_data_inside_current_dir(){
+        content_t temp= list_content();
+        //print content and done
+    }
     class dynamic_searcher_list{
-        dynamic_searcher_list()=default;
-        void add(list_filter_and_format::searcher element){
+        inline dynamic_searcher_list()=default;
+        inline void add(value_type element){
             storage.push_back(element);
         }
-        void output_search_and_discard_data_through_storage(){
+        inline void output_search_and_discard_data_through_storage(){
             for(auto dir : storage){
                 content_t temp= dir.list_content();
                 //print content and done
             }
         }
+
+        inline value_type& operator[](size_type index){
+            return storage[index];
+        }
     private:
         storage_t storage;
 
-        using storage_t=std::vector<list_filter_and_format::searcher>;
+        using storage_t=std::vector<value_type>;
         using value_type=storage_t::value_type;
         using size_type=storage_t::size_type;
     };
