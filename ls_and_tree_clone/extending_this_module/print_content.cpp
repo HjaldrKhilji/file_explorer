68c8db2a16b421e95205202e174fd046c2687171
Mueem <rusafkakhilji@gmail.com>
2026-09-14 23:02:02 +0500

    added print_content.cpp
    added print_content.cpp   to help print directory_content_t's content (aka the posix::dirent structure)

---
+++ ls_and_tree_clone/extending_this_module/print_content.cpp
@@ -0,0 +1,45 @@
+#include <print>
+namespace posix{
+    extern "C"{
+        #include <dirent.h>
+    }
+}
+
+export print_content
+export namespace print_content{
+    struct formated_data{
+        std::string filename_pre_fix;
+        std::string filename_post_fix;
+        std::string d_ino_post_fix;
+        std::string d_ino_post_fix;
+        std::string d_off_post_fix;
+        std::string d_off_post_fix;
+        std::string d_reclen;
+        std::string d_reclean_post_fix;
+        std::string d_type_post_fix;
+        std::string d_type_post_fix;
+    }
+    inline void print_content_without_pre_post_fixes_impl(posix:dirent* data_to_print){
+        std::println("{}/", data_to_print->d_name);
+        std::println("{}",  data_to_print->d_ino);
+        std::println("{}",  data_to_print->d_off);
+        std::println("{}",  data_to_print->d_reclen);
+        std::println("{}",  data_to_print->d_type);
+    }
+    inline void print_content_with_pre_post_fixes_impl(posix:dirent* data_to_print, formated_data& format){
+        std::println("{}{}{}", format.filename_pre_fix, data_to_print->d_name, format.filename_post_fix);
+        std::println("{}{}{}", format.d_ino_pre_fix, data_to_print->d_name, format.d_ino_post_fix);
+        std::println("{}{}{}", format.d_off_pre_fix, data_to_print->d_name, format.d_off_post_fix);
+        std::println("{}{}{}", format.d_reclen_pre_fix, data_to_print->d_name, format.d_reclen_post_fix);
+        std::println("{}{}{}", format.d_type_pre_fix, data_to_print->d_name, format.d_type_post_fix);
+    }
+    inline print_content(posix:dirent* data_to_print, bool with_custom_format, formated_data& format){
+        if(with_custom_format){
+            print_content_with_pre_post_fixes_impl(data_to_print, format);
+        }
+        else{
+            print_content_without_pre_post_fixes_impl(data_to_print, format);
+        }
+    }
+
+};
