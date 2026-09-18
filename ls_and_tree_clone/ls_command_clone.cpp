5d57a9dea463333263a10a31b435dfed5be4b6c6
Mueem <rusafkakhilji@gmail.com>
2026-09-18 23:58:09 +0500

    Refactor directory handling and imports in ls_command_clone

--- ls_and_tree_clone/ls_command_clone.cpp
+++ ls_and_tree_clone/ls_command_clone.cpp
@@ -1,32 +1,31 @@
+module;
 #include <iostream>
 #include <string>
 #include <array>
 #include <system_error>
 #include <print>
 #include <locale>
-
+#include <dirent.h>
+#include <fcntl.h>
+export module list_filter_and_format;
 //tree must be implemented in some other file and it would probably require a different file, where it tries to make use of the whole project in order to accomplish the desired task
-namespace posix{
-    extern "C"{
-        #include <dirent.h>
-        #include <fcntl.h>
+import print_content;
 
-    }
-    using filter_type=int (const struct dirent *)*;
-    int filter_non(const struct dirent *) {
+
+
+using filter_type=int*(const struct dirent *);
+int filter_non(const struct dirent *) {
         return 1;
     }
-    using ordering_type=int (const struct dirent **,const struct dirent **)*;
-    int total_no_op_order(const struct dirent **,const struct dirent **) {
+using ordering_type=int*(const struct dirent **,const struct dirent **);
+int total_no_op_order(const struct dirent **,const struct dirent **) {
         return -1;
     }
 
-}
-import print_content
-export module list_filter_and_format
+
 export namespace list_filter_and_format {
     struct directory_content_t{
-        posix::dirent **restrict dir_list;
+        dirent **restrict dir_list;
         std::size_t lenght;
         inline directory_content_t(directory_content_t&&)=default;
         inline void operator=(directory_content_t&&)=default;
@@ -43,7 +42,7 @@ export namespace list_filter_and_format {
     };
 inline directory_content_t list_content(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
         directory_content_t result;
-        result.lenght=posix::scandir(path.c_str(), &result.dir_list, filter_list[index_for_filter], ordering_list[index_for_ordering]);
+        result.lenght=scandir(path.c_str(), &result.dir_list, filter_list[index_for_filter], ordering_list[index_for_ordering]);
         return result;
     }
 inline directory_content_t list_content_while_checking_errors(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering){
@@ -65,11 +64,11 @@ class searcher{
         directory_fd=d;
         }
         inline driver(std::string path_name){
-            fd= posix::open(path_name.c_str(), O_DIRECTORY);
+            fd= open(path_name.c_str(), O_DIRECTORY);
         }
         inline directory_content_t list_content(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, formated_data& format){
             directory_content_t result;
-            result.lenght=posix::scandirat(directory_fd, path.c_str(), &result.dir_list, filter_list[index_for_filter], ordering_list[index_for_ordering]);
+            result.lenght=scandirat(directory_fd, path.c_str(), &result.dir_list, filter_list[index_for_filter], ordering_list[index_for_ordering]);
             return result;
         }
         inline directory_content_t list_content_while_checking_errors(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, formated_data& format){
