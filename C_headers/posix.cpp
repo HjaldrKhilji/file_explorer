95f395e313b557f9ab890a234601142210d3d6c3
Mueem <rusafkakhilji@gmail.com>
2026-09-19 15:48:40 +0500

    Add unistd.h and macros struct to posix.cpp

--- C_headers/posix.cpp
+++ C_headers/posix.cpp
@@ -4,5 +4,9 @@ export namespace posix{
     extern "C" {
         #include <dirent.h>
         #include <fcntl.h>
+        #include <unistd.h>
+        struct macros{
+        static constexpr auto o_directory=O_DIRECTORY;
+        };
     }
 }
