9ac615407a9d31de32ac4374702f1e677db2803d
Mueem <rusafkakhilji@gmail.com>
2026-09-20 18:14:39 +0500

    Refactor Driver class to use posix and print_content

--- main_driver/driver.cpp
+++ main_driver/driver.cpp
@@ -1,15 +1,20 @@
+module;
 #include <iostream>
 #include<string>
 import list_filter_and_format;
 import storage_for_driver;
+import print_content;
+import posix;
 export module driver;
 export namespace driver{
     class Driver{
     public:
-        inline driver(std::vector<std::string> paths):searcher_list{paths}
+        using search_size_t= storage_for_driver::dynamic_searcher_list::size_type;
+        using searcher_element_t= storage_for_driver::dynamic_searcher_list::value_type;
+        inline Driver(std::vector<std::string> paths):searcher_list{paths}
         {}
-        inline change_effective_working_dir(std::string dir){
-            chdir(dir.c_str());
+        inline void change_effective_working_dir(std::string dir){
+            posix::chdir(dir.c_str());
         }
         inline void add_searcher(std::string element){
             searcher_list.push_back(searcher_element_t{element});
@@ -18,24 +23,25 @@ export namespace driver{
             searcher_list.pop_back();
         }
         inline void clear(){
-            searcher_list.clear()
+            searcher_list.clear();
         }
         inline void add_searcher_and_change_working_dir(std::string element){
-            int fd= open(path_name.c_str(), O_DIRECTORY);
+            int fd= posix::open(element.c_str(), posix::macros::o_directory);
             searcher_list.push_back(searcher_element_t{fd});
-            fchdir(fd);
+            posix::fchdir(fd);
         }
-        inline void output_search(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, formated_data& format){
+        inline void output_search(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, print_content::formated_data& format){
             searcher_list.output_search(path, index_for_filter,  index_for_ordering,with_custom_format, format);
         }
-        inline void output_search_inside_current_dir(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, formated_data& format){
-            storage_for_driver::output_search_inside_current_dir(path, index_for_filter,  index_for_ordering,with_custom_format, format);
-        }
-        inline void output_search_while_checking_errors(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, formated_data& format){
+        inline void output_search_while_checking_errors(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, print_content::formated_data& format){
             searcher_list.output_search_while_checking_errors(path, index_for_filter,  index_for_ordering,with_custom_format, format);
         }
-        inline void output_search_inside_current_dir_while_checking_errors(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, formated_data& format){
-            storage_for_driver::output_search_inside_current_dir_while_checking_errors(path, index_for_filter,  index_for_ordering,with_custom_format, format);
+        inline void output_search_inside_current_dir(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, print_content::formated_data& format){
+            storage_for_driver::output_search(path, index_for_filter,  index_for_ordering,with_custom_format, format);
+        }
+
+        inline void output_search_inside_current_dir_while_checking_errors(std::string path, std::size_t index_for_filter, std::size_t index_for_ordering,bool with_custom_format, print_content::formated_data& format){
+            storage_for_driver::output_search_while_checking_errors(path, index_for_filter,  index_for_ordering,with_custom_format, format);
         }
         inline void change_an_entry(search_size_t index, searcher_element_t a){
             searcher_list[index]=a;
@@ -43,8 +49,6 @@ export namespace driver{
 
 
     private:
-        using searcher_element_t= storage_for_driver::dynamic_searcher_list::value_type;
-        using search_size_t= storage_for_driver::dynamic_searcher_list::size_type;
         storage_for_driver::dynamic_searcher_list searcher_list{};
 
     };
